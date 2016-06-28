77026ea73b8a2490ffd5eb8ed0c7cef06854f99d
Aurélien Matouillot <a.matouillot@gmail.com>
2016-06-28 17:54:35 +0200

    radio group

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,11 @@
 import {Component} from '@angular/core';
+import {RADIO_GROUP_DIRECTIVES} from "ng2-radio-group";
+
+console.log(RADIO_GROUP_DIRECTIVES);
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: '<h1>My First Angular 2 App</h1><input type="checkbox" [(ngModel)]="isSomethingEnabled"/>',
+    directives: [RADIO_GROUP_DIRECTIVES]
 })
 export class AppComponent { }
