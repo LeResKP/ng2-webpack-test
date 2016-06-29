171f911bd759645eeb5e92a636bf7adea3540820
Aurélien Matouillot <a.matouillot@gmail.com>
2016-06-29 07:51:42 +0200

    Working ng-radio-group example

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,7 +5,20 @@ console.log(RADIO_GROUP_DIRECTIVES);
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1><input type="checkbox" [(ngModel)]="isSomethingEnabled"/>',
+    template: `<h1>My First Angular 2 App updated</h1>
+
+            <input type="checkbox" [(ngModel)]="isSomethingEnabled"/>
+
+           <checkbox-group #orderByCheckboxGroup="ngForm" [(ngModel)]="orderBy" [required]="true">
+            <checkbox-item value="rating">Rating</checkbox-item>
+            <checkbox-item value="date">Date</checkbox-item>
+            <checkbox-item value="watches">Watch count</checkbox-item>
+            <checkbox-item value="comments">Comment count</checkbox-item>
+        </checkbox-group>
+    `,
     directives: [RADIO_GROUP_DIRECTIVES]
 })
-export class AppComponent { }
+export class AppComponent {
+    isSomethingEnabled: boolean = true;
+    orderBy: string[] = ["rating", "comments"];
+}
